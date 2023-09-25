82643d58fb3489181fab3ca3f7f942e3dd64eefc
kaustubh812 <watanekaustubh@gmail.com>
2023-09-25 20:23:56 +0530

    code updated

--- script.js
+++ script.js
@@ -1,75 +1,78 @@
-let foodinfo = require('./food.json')
+const sample = require('./food.json');
 
-//list all the food items
-let all = foodinfo.forEach(element => {
-    console.log(element)
-});
+// list all the food items
+let foods = sample.map((items) =>{ 
+          return items.foodname;
+})
+console.log(foods);
 
-
-//list all the food items with category vegetables
-let Vegetable = foodinfo.filter((item) =>{
-    if (item.category == "Vegetable") console.log(item);
-});
-
-
+// list all the food items with category vegetables
+function allVegetables(value) {
+    return value.filter(item=> item.category === "Vegetable").map(item=>item.foodname)
+}
+const result1 = allVegetables(sample)
+console.log(result1);
 
 //list all the food items with category fruit
-let fruit = foodinfo.filter((item) =>{
-    if (item.category == "Fruit") console.log(item);
-});
+function allFruits(value) {
+   return value.filter(item => item.category === "Fruit").map(item => item.foodname); 
+}
+const result2 = allFruits(sample);
+console.log(result2);
 
-
-//list all the food items with category protien
-let protein = foodinfo.filter((item) =>{
-    if(item.category == "Protein") console.log(item);
-});
-
+// list all the food items with category protien
+function allproteins(value) {
+   return value.filter(item => item.category === "Protein").map(item => item.foodname);
+}
+const result3 = allproteins(sample);
+console.log(result3);
 
 //list all the food items with category nuts
-let nuts = foodinfo.filter((item) =>{
-    if(item.category == "Nuts") console.log(item);
-});
+function allNuts(value) {
+  return value.filter(item => item.category === "Nuts").map(item => item.foodname);
+}
+const result4 = allNuts(sample);
+console.log(result4);
 
+// list all the food items with category grains
+function allGrains(value) {
+    return value.filter(item => item.category === "Grain").map(item => item.foodname);
+}
+const result5 = allGrains(sample);
+console.log(result5);
 
-//list all the food items with category grains
-let grains = foodinfo.filter((item) => {
-    if (item.category == "Grain") console.log(item);
-});
+// list all the food items with category dairy
+function allDairy(value) {
+  return value.filter(item => item.category === "Dairy").map(item => item.foodname);
+}
+const result6 = allDairy(sample);
+console.log(result6);
 
+// list all the food items with calorie above 100
+function calorieAbove(value) {
+   return value.filter(item => item.calorie >=100 ).map(item => item.foodname); 
+}
+const result7 = calorieAbove(sample);
+console.log(result7);
 
-//list all the food items with category dairy
-let dairy = foodinfo.filter((item) =>{
-    if (item.category == "Dairy") console.log(item);
-});
+// list all the food items with calorie below 100
+function calorieBelow(value) {
+   value.filter(item => item.calorie <= 100 ).map(item => item.foodname);
+}
+const result8 = calorieBelow(sample);
+console.log(result8);
 
-
-//list all the food items with calorie above 100
-let calorie_above = foodinfo.filter((item) => {
-    if (item.calorie > 100) console.log(item);
-});
-
-
-//list all the food items with calorie below 100
-let calorie_below = foodinfo.filter((item) => {
-    if(item.calorie < 100) console.log(item);
-});
-
-
-//list all the food items with highest protien content to lowest
-function highest() {
-    foodinfo.sort((a, b) => b.protiens - a.protiens);
-    foodinfo.forEach((item) => {
-      console.log(item);
-    });
+// list all the food items with highest protien content to lowest
+function highestProtienToLow(value) {
+    return value.slice().sort((a, b) => b.protiens - a.protiens).map(item => item.foodname);
   }
-  highest()
+ const result9 = highestProtienToLow(sample);
+console.log(result9);
 
+//  list all the food items with lowest cab content to highest
+function lowestCabToHigh(value) {
+    return value.slice().sort((a, b) => a.cab - b.cab).map(item => item.foodname);
+  }
+const result10 = lowestCabToHigh(sample);
+console.log(result10);
   
-  //list all the food items with lowest cab content to highest
-  function lowest() {
-    foodinfo.sort((a, b) => a.cab - b.cab);
-    foodinfo.forEach((item) => {
-      console.log(item);
-    });
-  }
-  lowest()
